827635935d26cf9e0f9d01ff3c84143842514475
John Williamson <johnhw@gmail.com>
2016-02-11 09:44:05 +0000

    Added documentation to pseudo.py methods

--- pseudo.py
+++ pseudo.py
@@ -7,7 +7,14 @@ consonants = "".join(set(alphabet) - set(vowels))
 
 def pron_encode(hx, off=0):
     """Encode a hex string as a pronouncable letter sequence. 
-    If off=0, starts with a consonant, off=1 to start with a vowel"""
+    
+    Parameters:
+        off: If off=0, starts with a consonant, off=1 to start with a vowel
+        hx: Hex string to be encoded
+        
+    Returns:
+        A pronouncable string (i.e alternating consonant/vowel)
+    """
     i = int(hx, 16)
     j = off
     out = []
@@ -24,20 +31,51 @@ def pron_encode(hx, off=0):
     return "".join(out)
         
 def check_pseudo(user_id, n=3, off=0):
-    """Create a pronouncable hash of a string"""
+    """Create a pronouncable hash of a string.
+    
+    Parameters:
+        user_id:    The id to check
+        n:          Number of check digits to generate
+        off:        Offset to be passed to pron_encode()
+        
+    Returns:
+        check: Pronouncable check digit string (e.g. "GAR")
+    
+    """
     sha = hashlib.sha256()
     sha.update(user_id)   
     return pron_encode(sha.hexdigest(),off=off)[:n]
 
 def verify_pseudo(user_id, check=3):
-    """Verify that the pseudonym is valid (i.e. not mistyped)"""
+    """Verify that the pseudonym is valid (i.e. not mistyped).
+    
+    Parameters:
+        user_id:    pronouncable user ID to check
+        check:      Number of check digits used (last n characters of the string)
+        
+    Returns:
+        match: True if the check digits match, False otherwise
+    
+    """
     uid =  user_id.replace("-","")    
     n = len(uid) - check    
     return check_pseudo(uid[:-check], n=check, off=n%2) == uid[-check:]
 
 def get_pseudo(n=7, split=5, check=3):
     """Return a pseudonym of the form ABC-DE, where ABC are random letters, and DE are check
-    digits"""    
+    digits.
+    
+    
+    Parameters:
+        n:  Number of letters in the random pseudonym portion
+        split: Interval to split string at with a "-" (e.g. 3 would produce strings like ABC-DEF-GHI)
+        check:  Number of check digits to append
+    
+    Returns:
+        pseudo: Pronounceable pseudonym         
+    
+    
+    """    
     uid = random.getrandbits(256)
     user_id = check_pseudo("%x"%uid, n=n)        
     check = check_pseudo(user_id, n=check, off=n%2)    
