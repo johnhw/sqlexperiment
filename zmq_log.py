64c9ebd2c5228f805775f68b43bffebcfb6c2672
johnhw <johnhw@gmail.com>
2016-02-17 14:24:49 +0000

    Fixes to the ZMQ implementation

--- zmq_log.py
+++ zmq_log.py
@@ -1,9 +1,11 @@
 import time
 import sys
+import logging
 from multiprocessing import Process, Lock, Queue, Pipe, RLock
 import experimentlog
 import zmq
 from experimentlog import MetaProxy
+import traceback
 
 # Port used for ZMQ communication
 ZMQ_PORT = 3149
@@ -27,10 +29,11 @@ def start_experiment(args, kwargs):
                 retval = fn
             # send back the return value                
             socket.send_pyobj((True, retval), protocol=-1)
-        except Exception, exc:            
+        except:            
             # exception, return the full exception info
-            info = sys.exc_info()
-            socket.send_pyobj((False, info), protocol=-1)
+            info = sys.exc_info()        
+            tb = "\n".join(traceback.format_exception(*info, limit=20))            
+            socket.send_pyobj((False, (info[1], tb)), protocol=-1)
         # update stopped flag
         stopped = not e.in_run        
 
@@ -44,6 +47,7 @@ class LogProxy(object):
         context = zmq.Context()
         self.socket = context.socket(zmq.REQ)
         self.socket.connect("tcp://localhost:%s" % ZMQ_PORT)
+        # make metadata work the same way as in the ExperimentLog
         self.meta = MetaProxy(self)    
     
     def __getattr__(self, attr):             
@@ -58,6 +62,7 @@ class LogProxy(object):
                 return value
             else:
                 # deal with exceptions in the remote process
+                logging.error(value[1])
                 raise value[0]
         else:
             # redirect calls to the remote object
@@ -68,23 +73,29 @@ class LogProxy(object):
                     return value
                 else:
                     # deal with exceptions in the remote process
+                    logging.error(value[1])
                     raise value[0]
+                    
             return proxy
             
 
 class ZMQLog(object):
     def __init__(self, *args, **kwargs):
+        """Start the log server. Objects to access the server are produced by get_proxy()"""
         self.process = Process(target=start_experiment, args=(args, kwargs))
         self.process.start()
         
     def get_proxy(self):
+        """Return a proxy object which behaves like ExperimentLog, but actually
+        redirects to a remote process with a single ExperimnetLog object"""
         return LogProxy()
     
         
 if __name__=="__main__":
+    # basic test if this is working...
     m = ZMQLog("my_multi.db", ntp_sync=False)
     proxy = m.get_proxy()
     while 1:
-        proxy.log("mouse", data={"x":5, "y":6})
+        proxy.log('mouse')
         time.sleep(1)        
         print proxy.meta.stage
